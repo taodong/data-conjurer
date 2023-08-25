90a2b126118434bdb1e3eff7339082898f252ee4
Tao Dong <taojdcn@gmail.com>
2023-08-24 19:28:23 -0700

    Reference implementation

--- conjurer-common/src/main/java/tao/dong/dataconjurer/common/model/EntityWrapper.java
+++ conjurer-common/src/main/java/tao/dong/dataconjurer/common/model/EntityWrapper.java
@@ -1,18 +1,18 @@
 package tao.dong.dataconjurer.common.model;
 
 import jakarta.validation.constraints.NotNull;
-import tao.dong.dataconjurer.common.support.IndexValueGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.set.ListOrderedSet;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Slf4j
 class EntityWrapper {
     private final DataEntity entity;
     private final Set<String> dependencies = new HashSet<>();
@@ -24,14 +24,12 @@ class EntityWrapper {
      */
     private final AtomicInteger status = new AtomicInteger(0);
     private final long count;
-    private final int initSize;
 
     private final Map<String, Set<TextValue>> references = new HashMap<>();
 
     public EntityWrapper(@NotNull DataEntity entity, EntityData data) {
         this.entity = entity;
         this.count = data.count();
-        this.initSize = this.count > 1000 ? 1000 : (int)this.count;
         var deps = entity.properties()
                 .stream()
                 .map(EntityProperty::reference)
@@ -55,13 +53,40 @@ class EntityWrapper {
         return !dependencies.isEmpty();
     }
 
-    public void addReference(String... properties) {
+    public void createReference(String... properties) {
         if (properties != null) {
             for (var prop : properties) {
-                references.computeIfAbsent(prop, ignore -> new HashSet<>(initSize));
+                references.computeIfAbsent(prop, ignore -> new ListOrderedSet<>());
             }
         }
     }
 
+    public void saveReference(@NotNull Map<String, TextValue> refs) {
+        for (var entry : refs.entrySet()) {
+            if (references.containsKey(entry.getKey())) {
+                references.get(entry.getKey()).add(entry.getValue());
+            } else {
+                LOG.debug("Save unknown reference value {}.{}", entity.name(), entry.getKey());
+            }
+        }
+    }
 
+    public int getReferenceSize(String name) {
+        if (references.containsKey(name)) {
+            return references.get(name).size();
+        }
+        return 0;
+    }
+
+    public TextValue getReferenceValue(String name, int index) {
+        try {
+            if (references.containsKey(name)) {
+                return ((ListOrderedSet<TextValue>) references.get(name)).get(index);
+            }
+            return null;
+        } catch (IndexOutOfBoundsException e) {
+            LOG.debug("Index calculation error found", e);
+            return null;
+        }
+    }
 }
