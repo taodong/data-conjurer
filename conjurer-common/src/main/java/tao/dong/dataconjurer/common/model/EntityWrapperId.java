813c3b9a52be4ee6d4326aa5169e84405e543be7
Tao Dong <taojdcn@gmail.com>
2023-11-05 17:41:55 -0800

    Add sorting to EntityWrapperId

--- conjurer-common/src/main/java/tao/dong/dataconjurer/common/model/EntityWrapperId.java
+++ conjurer-common/src/main/java/tao/dong/dataconjurer/common/model/EntityWrapperId.java
@@ -1,7 +1,19 @@
 package tao.dong.dataconjurer.common.model;
 
-public record EntityWrapperId(String entityName, int dataId) {
+import java.util.Comparator;
+import java.util.Objects;
+
+public record EntityWrapperId(String entityName, int dataId) implements Comparable<EntityWrapperId>{
     public String getIdString() {
         return entityName + "_" + dataId;
+    }
+
+    @Override
+    public int compareTo(EntityWrapperId that) {
+        if (that != null) {
+            return Objects.compare(this, that, Comparator.comparing(EntityWrapperId::entityName).thenComparing(EntityWrapperId::dataId));
+        } else {
+            return -1;
+        }
     }
 }
---
+++ conjurer-common/src/test/java/tao/dong/dataconjurer/common/model/EntityWrapperIdTest.java
@@ -0,0 +1,33 @@
+package tao.dong.dataconjurer.common.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EntityWrapperIdTest {
+
+    @Test
+    void testGetIdString() {
+        var id = new EntityWrapperId("t1", 1);
+        assertEquals("t1_1", id.getIdString());
+    }
+
+    private static Stream<Arguments> testCompareTo() {
+        return Stream.of(
+                Arguments.of(new EntityWrapperId("t1", 0), new EntityWrapperId("t1", 0), 0),
+                Arguments.of(new EntityWrapperId("t1", 0), new EntityWrapperId("t1", 1), -1),
+                Arguments.of(new EntityWrapperId("t1", 1), new EntityWrapperId("t0", 2), 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testCompareTo(EntityWrapperId id1, EntityWrapperId id2, int expected) {
+        assertEquals(expected, id1.compareTo(id2));
+    }
+}
