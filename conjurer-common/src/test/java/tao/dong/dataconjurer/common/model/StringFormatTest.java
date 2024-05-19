1fe9a66d2db03e52d15e77e4ff1728ab43bd6bea
Tao Dong <taojdcn@gmail.com>
2024-05-18 22:31:40 -0700

    Add unit test

--- conjurer-common/src/test/java/tao/dong/dataconjurer/common/model/StringFormatTest.java
+++ conjurer-common/src/test/java/tao/dong/dataconjurer/common/model/StringFormatTest.java
@@ -1,10 +1,55 @@
 package tao.dong.dataconjurer.common.model;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringFormatTest {
+
+    private final ObjectMapper objectMapper = new YAMLMapper();
+
+    private static Stream<Arguments> testYamlCreator() {
+        return Stream.of(
+                Arguments.of("""
+                            type: "format"
+                            format: "ref-##?##"
+                        """, new StringFormat("ref-##?##")),
+                Arguments.of("""
+                            type: "format"
+                            format: |
+                                prefix:
+                                    - "prefix-1"
+                                    - "prefix-2"
+                                suffix:
+                                    - "suffix-1"
+                                    - "suffix-2"
+                        """,
+                        new StringFormat(
+                                """
+                                prefix:
+                                    - "prefix-1"
+                                    - "prefix-2"
+                                suffix:
+                                    - "suffix-1"
+                                    - "suffix-2"
+                                """
+                        ))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testYamlCreator(String input, StringFormat expected) throws Exception {
+        var actual = objectMapper.readValue(input, Constraint.class);
+        assertEquals(expected, actual);
+    }
 
     @Test
     void testGetType() {
